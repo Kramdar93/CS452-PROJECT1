cdca449259b7c78f6bfbfea64c13e80cf9c2c189
Mark Elsinger <thesmokingamp@gmail.com>
2014-03-25 21:26:39 -0400

    Update fragmentShader.glsl

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -1,18 +1,20 @@
 #version 130
 
 //in vec4 color;
-//out vec4 fColor;
+out vec4 fColor;
 in vec2 texcoord;
 
-uniform sampler2D texture;
+uniform sampler2D s_fTexture;
+uniform vec4 color;
 
 void main () {
 	// pass through the given color
-	//fColor = color;
+	fColor = color;
 
 	//solid color
 	//fColor = vec4(0.5f, 0.5, 0.5f, 1.0f);
 
 	//textures!
-	gl_FragColor = texture2D(texture, texcoord);
+	//gl_FragColor = texture2D(s_fTexture, texcoord);
+	//fColor = texture2D(s_fTexture, texcoord);
 }
